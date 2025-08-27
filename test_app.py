1efd27c13745bfb520db856808b3794dcdec1b08
Ligdas <dligdas@active.gr>
2025-08-27 14:39:13 +0300

    Add test print to app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3, 4), 7)
+        self.assertEqual(add(3, 4), 8)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
